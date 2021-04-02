41a90e069e0175e06486f65f674f542097a89746
sxxRaul <975547545@qq.com>
2021-04-02 19:34:28 +0800

    feat: listen

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 import EventEmitter from './lib/emitter'
 
+interface ReplicateOptions {
+
+}
+
+/**
+ * @description Replicate
+ */
 class Replicate extends EventEmitter {
-  
+  private trigger: HTMLElement | HTMLCollection;
+
+  constructor (trigger: HTMLElement | HTMLCollection, options) {
+    super()
+    
+    this.listen(trigger)
+  }
+
+  listen (trigger: HTMLElement | HTMLCollection) {
+    trigger.addEventListener('click', e => this.clickListener(e))
+  }
+
+  clickListener (e: Event) {
+
+  }
 }
